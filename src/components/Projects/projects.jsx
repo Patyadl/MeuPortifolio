b546aeafc8b0aa4998424dad3184b822f0971c98
Patyadl <patricia.00000848012@unicap.br>
2024-09-30 17:36:45 -0300

    modificando estilo de projetos. criado constantes para gerenciamento de estado

--- src/components/Projects/projects.jsx
+++ src/components/Projects/projects.jsx
@@ -1,36 +1,66 @@
-import React from "react";
+import React, {useState} from "react";
 import Nav from "../NavBar/nav";
 import "../Projects/projects.css"
-const Project = () => {return (
-    <div>
-        <Nav />
-    
-     <section className={"projetoStart"}>
-        <div className={"start"}></div>
-     <div className={"projetoNear"}></div>
-     <h1 id={"txt"}>Participei do projeto Start em 2022 e foi a minha primeira experiência com o mundo 
-     da programação. Um projeto bastante enriquecedor, em parceria com a Rede Cidadã e outras empresas
-     como a Accenture. Lá aprendi um pouco de tudo: Lógica de programação, Html, Css, Java, Fazer amigos
-     e construir um projeto.</h1>
-     </section>
+const Project = () => {
 
-     <section className={"universidade"}>
-    <h1 id={"txt"}>Em 2022.2 entrei a Universidade Católica de Pernambuco e estou cursando o quarto período
-    de Sistemas Para Internet.</h1>
+    const [overlayContent, setOverlayContent] = useState("")
+    const [isOverlayVisible, setOverlayVisible] = useState(false)
+
+    const handleOverlay = (props) => {
+        setOverlayContent(props)
+        setOverlayVisible(true)
+    }
+
+    const closeOverlay = () => {
+        setOverlayVisible(false);
+    }
+
+    return (
+    <div>
+
+        <Nav />
+        <div className="Projetos">
+
+     <section className={"projetoStart-container"} onClick={() => handleOverlay("Participei do projeto Start em 2022 e foi a minha primeira experiência com o mundo da programação. Um projeto bastante enriquecedor, em parceria com a Rede Cidadã e outras empresas como a Accenture. Lá aprendi um pouco de tudo: Lógica de programação, Html, Css, Java, Fazer amigos e construir um projeto.")}>
+                <div className="projetoStart">
+
+                <div className={"start"}></div>
+                <div className={"projetoNear"}></div>
+                </div>
+                <h1 id={"txt"}>Participei do projeto Start em 2022...</h1>
+            </section>
+
+     <section className={"universidade-container"} onClick={ () => handleOverlay( " Em 2022.2 entrei na Universidade Católica de Pernambuco e estou cursando o quinto período de Sistemas Para Internet.")}>
+ 
+
+    <h1 id={"txt"}>Em 2022.2 entrei na UNICAP...</h1>
+    <div className="universidade">
     <div className={"embarque"}></div>
     <div className={"unicap"}></div>
+    </div>
      </section>
 
-     <section className="universidade-projetos">
-     <h1 id="txt">Durante os períodos, pude participar do desenvolvimento de outros projetos que super
-                me enriqueceram profissionalmente.</h1>
+     <section className="universidade-projetos" onClick={ () => handleOverlay( " Durante os períodos, pude participar do desenvolvimento de outros projetos que super me enriqueceram profissionalmente.")}>
+     <h1 id="txt">Durante os períodos...</h1>
                 <div className="images">
                     
                     <div className="projetoBora"></div>
                     <div className="projetoConfeit"></div>
+                    
                 </div>
+                
                 <div className="projetoFC"></div>
+               
             </section>
+            </div>
+            {isOverlayVisible && (
+                 <div id="overlay" className="overlay">
+                 <div className="overlay-content">
+                     <span id="close-btn" onClick={closeOverlay}>&times;</span>
+                     <p>{overlayContent}</p>
+                 </div>
+                 </div>
+            )}
     </div>
 )}
 export default Project
