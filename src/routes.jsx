f7a18c7083aa25374c8cc751f4cb89f935b26d7c
Patyadl <patricia.00000848012@unicap.br>
2024-12-20 01:37:12 -0300

    apagando rotas

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,9 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './components/Home/home'
 import Project from './components/Projects/projects'
 import Estudos from './components/Estudos/estudos'
-import Dicas from './components/Dicas/dicas'
 import Livros from './components/Livros/livros'
-import Sobre from './components/SobreMim/sobre'
 
 function Main() {
   return (
@@ -13,9 +11,8 @@ function Main() {
         <Route path="/" element={<Home />} />
       <Route path="/projetos" element={<Project/>} />
       <Route path="/estudos" element={<Estudos/>} />
-      <Route path="/dicas" element={<Dicas/>} />
       <Route path="/livros" element={<Livros/>} />
-      <Route path="/sobre" element={<Sobre/>} />
+
       </Routes>
     </Router>
   )
