cfed74e04630653f23c65ccd2d1c3310c16081d0
Patyadl <patricia.00000848012@unicap.br>
2025-12-31 02:54:39 -0300

    mudando o formato do menu da home em mobile

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -6,53 +6,46 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div className="container-home">
-      <div className="figuras_direita">
-        <div className="foguete"></div>
-      </div>
-      
-      <div className="figuras_esquerda">
-        <div className="iconeReact"></div>
-        <div className="iconeHeadphone"></div>
-        <div className="iconeCafe"></div>
-      </div>
 
-      <div className="container">
-        <div className="menu">
-          <h3>Meu <br /> portifólio</h3> 
-           <h5><Link to= "/jornada" className={"link"}> Minha jornada </Link></h5>
-          <h5><Link to= "/projetos" className={"link"}> Projetos </Link></h5>
-          <h5 id={"menu"}><Link to= "/livros" className={"link"}> Livros favoritos </Link></h5>
-          <h5 id={"menu"}><Link to= "/cargo" className={"link"}> Cargo atual </Link></h5>
-          <h5 id={"menu"}><Link to= "/contatos" className={"link"}> Contatos </Link></h5>
-         
-        </div>
+  <div className="container">
+
    
-        <div className="Centralizar_minhaFoto">
-          <div className="minhaFoto"></div>
-        </div>
-        <h4 id="frase">#pensamentoCrítico</h4>
-
-
-<div className="centralizar-linguagens">
-    <div className="linguagens_ferramentas">
-  <div className="linguagens_ferramentas_1">
-    <div className="java"></div>
-    <div className="spring"></div>
-    <div className="js"></div>
-  </div>
-
-  <div className="astronauta"></div>
-
-  <div className="linguagens_ferramentas_2">
-    <div className="react"></div>
-    <div className="tailwind"></div>
-    <div className="postgre"></div>
-  </div>
-</div>
-</div>
-        
+    <section className="topo">
+      <div className="menu">
+        <h3>Meu <br /> portifólio</h3> 
+        <h5><Link to="/jornada" className="link">Minha jornada</Link></h5>
+        <h5><Link to="/projetos" className="link">Projetos</Link></h5>
+        <h5><Link to="/livros" className="link">Livros favoritos</Link></h5>
+        <h5><Link to="/cargo" className="link">Cargo atual</Link></h5>
+        <h5><Link to="/contatos" className="link">Contatos</Link></h5>
       </div>
-    </div>
+
+      <div className="Centralizar_minhaFoto">
+        <div className="minhaFoto"></div>
+      </div>
+    </section>
+
+ 
+    <section className="centralizar-linguagens">
+      <div className="linguagens_ferramentas">
+        <div className="linguagens_ferramentas_1">
+          <div className="java"></div>
+          <div className="spring"></div>
+          <div className="js"></div>
+        </div>
+
+        <div className="astronauta"></div>
+
+        <div className="linguagens_ferramentas_2">
+          <div className="react"></div>
+          <div className="tailwind"></div>
+          <div className="postgre"></div>
+        </div>
+      </div>
+    </section>
+
+  </div>
+</div>
   );
 };
 
